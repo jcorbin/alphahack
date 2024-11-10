99bb2d80b20c2dd8d86dfffa9be23c808a77ab53
Joshua T Corbin <jcorbin@wunjo.org>
2024-11-10 11:25:58 -0500

    Add general prompt analysis

--- review.py
+++ review.py
@@ -51,8 +51,33 @@ class Questioned:
                 break
         return cls(float(t), int(lo), int(q), int(hi), word, canon_resp)
 
+@dataclass
+class Prompt:
+    time: float
+    resp: str
+
+    pattern = re.compile(r'''(?x)
+        T (?P<time> \d+ (?: \. \d+ )? )
+
+        \s+
+        >
+        \s*
+
+        (?P<resp> .* )
+
+        $
+    ''')
+
+    @classmethod
+    def match(cls, line):
+        match = cls.pattern.match(line)
+        if not match: return None
+        t, resp = match.groups()
+        return cls(float(t), resp)
+
 def analyze(lines):
     quest = []
+    prompt = []
 
     for line in lines:
         line = line.rstrip('\r\n')
@@ -62,18 +87,36 @@ def analyze(lines):
             quest.append(q)
             continue
 
+        p = Prompt.match(line)
+        if p is not None:
+            prompt.append(p)
+            continue
+
     quest = sorted(quest, key = lambda x: x.time)
+    prompt = sorted(prompt, key = lambda x: x.time)
 
     def merge():
         qi = 0
+        pi = 0
         while True:
             qt = quest[qi].time if qi < len(quest) else None
-            if qt is None:
+            pt = prompt[pi].time if pi < len(prompt) else None
+            if qt is None and pt is None:
                 break
 
-            qr = quest[qi]
-            qi += 1
-            yield qt, qr
+            if pt is None or qt <= pt:
+                qr = quest[qi]
+                qi += 1
+                yield qt, qr, None
+                continue
+
+            if qt is None or pt < qt:
+                pr = prompt[pi]
+                pi += 1
+                yield pt, None, pr
+                continue
+
+            raise RuntimeError('unreachable merge state')
 
     max_ix = max(max(r.lo, r.q, r.hi) for r in quest)
     t_width = max(4, max(len(f'{r.time:.1f}') for r in quest))
@@ -85,7 +128,7 @@ def analyze(lines):
 
     yield f'T{"time":{t_width}} {"ΔT":>{t_width}} [ {"lo":{ix_width}} : {"query":{ix_width}} : {"hi":{ix_width}} ] {"<word>":{word_width}}? response ... analysis'
 
-    for t, qn in merge():
+    for t, qn, pr in merge():
         dt = t - prior_t
 
         if qn is not None:
@@ -93,6 +136,10 @@ def analyze(lines):
             m = math.floor(qn.hi/2+qn.lo/2)
             b = qn.q - m
             yield f'T{t:{t_width}.1f} {dt:{t_width}.1f} [ {qn.lo:{ix_width}} : {qn.q:{ix_width}} : {qn.hi:{ix_width}} ] {qn.word:{word_width}}? {qn.resp:{resp_width}} ... wid:{w:{ix_width}} mid:{m:{ix_width}} bias:{b}'
+
+        elif pr is not None:
+            # TODO extract and report viewing window telemetry
+            yield f'T{t:{t_width}.1f} {dt:{t_width}.1f} > {pr.resp}'
 
         prior_t = t
 
