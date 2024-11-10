41bac0543c56068e834b1fcb7a0a613ae204b1f1
Joshua T Corbin <jcorbin@wunjo.org>
2024-11-10 11:19:36 -0500

    Refactor analysis line extraction

--- review.py
+++ review.py
@@ -6,7 +6,7 @@ import sys
 from dataclasses import dataclass
 
 @dataclass
-class Response:
+class Questioned:
     time: float
     lo: int
     q: int
@@ -52,35 +52,47 @@ class Response:
         return cls(float(t), int(lo), int(q), int(hi), word, canon_resp)
 
 def analyze(lines):
-    responses = []
+    quest = []
 
     for line in lines:
         line = line.rstrip('\r\n')
 
-        r = Response.match(line)
-        if r is not None:
-            responses.append(r)
+        q = Questioned.match(line)
+        if q is not None:
+            quest.append(q)
             continue
 
+    quest = sorted(quest, key = lambda x: x.time)
 
-    max_ix = max(max(r.lo, r.q, r.hi) for r in responses)
-    t_width = max(4, max(len(f'{r.time:.1f}') for r in responses))
+    def merge():
+        qi = 0
+        while True:
+            qt = quest[qi].time if qi < len(quest) else None
+            if qt is None:
+                break
+
+            qr = quest[qi]
+            qi += 1
+            yield qt, qr
+
+    max_ix = max(max(r.lo, r.q, r.hi) for r in quest)
+    t_width = max(4, max(len(f'{r.time:.1f}') for r in quest))
     ix_width = max(5, len(str(max_ix)))
-    word_width = max(6, max(len(r.word) for r in responses))
-    resp_width = max(8, max(len(r.resp) for r in responses))
+    word_width = max(6, max(len(r.word) for r in quest))
+    resp_width = max(8, max(len(r.resp) for r in quest))
 
     prior_t = 0
 
     yield f'T{"time":{t_width}} {"ΔT":>{t_width}} [ {"lo":{ix_width}} : {"query":{ix_width}} : {"hi":{ix_width}} ] {"<word>":{word_width}}? response ... analysis'
 
-    for r in responses:
-        t = r.time
+    for t, qn in merge():
         dt = t - prior_t
 
-        w = r.hi - r.lo
-        m = math.floor(r.hi/2+r.lo/2)
-        b = r.q - m
-        yield f'T{t:{t_width}.1f} {dt:{t_width}.1f} [ {r.lo:{ix_width}} : {r.q:{ix_width}} : {r.hi:{ix_width}} ] {r.word:{word_width}}? {r.resp:{resp_width}} ... wid:{w:{ix_width}} mid:{m:{ix_width}} bias:{b}'
+        if qn is not None:
+            w = qn.hi - qn.lo
+            m = math.floor(qn.hi/2+qn.lo/2)
+            b = qn.q - m
+            yield f'T{t:{t_width}.1f} {dt:{t_width}.1f} [ {qn.lo:{ix_width}} : {qn.q:{ix_width}} : {qn.hi:{ix_width}} ] {qn.word:{word_width}}? {qn.resp:{resp_width}} ... wid:{w:{ix_width}} mid:{m:{ix_width}} bias:{b}'
 
         prior_t = t
 
