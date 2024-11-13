aa6e2c0f0eb297c15ca5833a8197066fb80df51e
Joshua T Corbin <jcorbin@wunjo.org>
2024-11-13 14:22:41 -0500

    Annotate scrape script

--- scrape.py
+++ scrape.py
@@ -1,19 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+from collections.abc import Generator
+from typing import cast
 
-def scrape_links(url):
+def scrape_links(url: str):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     for a in soup.find_all('a', class_='letter'):
-        yield a.text, urljoin(url, a['href'])
+        link_text = cast(str, a.text)
+        link_href = cast(str, a['href'])
+        link_url = urljoin(url, link_href)
+        yield link_text, link_url
 
-def scrape_words(url):
+def scrape_words(url: str) -> Generator[str]:
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     for word in soup.find_all(class_='word'):
-        yield word.text
+        yield cast(str, word.text)
 
-for url in sorted(set(url for text, url in scrape_links('https://www.scrabblehelper.nl/woordenlijst'))):
+for url in sorted(set(url for _text, url in scrape_links('https://www.scrabblehelper.nl/woordenlijst'))):
     for word in scrape_words(url):
         print(word)
