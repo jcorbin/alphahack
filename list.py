3eafacc96730ffdf877360b6bfbfed397e740d12
Joshua T Corbin <jcorbin@wunjo.org>
2024-11-13 14:23:17 -0500

    Annotate list script

--- list.py
+++ list.py
@@ -1,33 +1,37 @@
 #!/usr/bin/env python
 
 import argparse
-import hashlib
-import math
+from collections.abc import Sequence
+from typing import cast, TextIO
+
 from hack import WordList
 
-def note(s):
+Note = tuple[int, str]
+
+def note(s: str) -> Note:
     si, _, mark = s.partition(':')
     i = int(si)
     return (i, mark or '???')
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--wordfile', type=argparse.FileType('r'), default='alphalist.txt')
-parser.add_argument('lo', type=int)
-parser.add_argument('hi', type=int)
-parser.add_argument('note', type=note, nargs='*')
+_ = parser.add_argument('--wordfile', type=argparse.FileType('r'), default='alphalist.txt')
+_ = parser.add_argument('lo', type=int)
+_ = parser.add_argument('hi', type=int)
+_ = parser.add_argument('note', type=note, nargs='*')
 
 args = parser.parse_args()
 
-notes = dict(args.note)
-wordlist = WordList(args.wordfile)
+notes = dict(cast(Sequence[Note], args.note))
+wordlist = WordList(cast(TextIO, args.wordfile))
 print(wordlist.describe)
 words = wordlist.words
 
-word_width = max(
-    len(words[i])
-    for i in range(args.lo, args.hi))
+word_range = range(cast(int, args.lo), cast(int, args.hi))
 
-for i in range(args.lo, args.hi): 
-    note = notes.get(i) or ''
-    print(f'{i} {words[i]:{word_width}}{" <-- " if note else ""}{note}')
+word_width = max(len(words[i]) for i in word_range)
+
+for i in word_range: 
+    i_note = notes.get(i, '')
+    i_mark = f' <-- {i_note}' if i_note else ''
+    print(f'{i} {words[i]:{word_width}}{i_mark}')
 
