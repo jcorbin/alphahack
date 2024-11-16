6537fb04fa2015592d238edbe08576ac14a6bd71
Joshua T Corbin <jcorbin@wunjo.org>
2024-11-16 11:18:57 -0500

    Factor review.{SearchLog out of analyze}

--- review.py
+++ review.py
@@ -18,11 +18,8 @@ class Questioned:
     resp: str
 
     pattern = re.compile(r'''(?x)
-        # T4.0846012760011945
-        T (?P<time> \d+ (?: \. \d+ )? )
-
         # [0 : 98266 : 196598]
-        \s+ \[ \s*
+        \s* \[ \s*
             (?P<lo> \d+ )
             \s* : \s*
             (?P<q> \d+ )
@@ -43,16 +40,16 @@ class Questioned:
     ''')
 
     @classmethod
-    def match(cls, line: str):
+    def match(cls, t: float, line: str):
         match = cls.pattern.match(line)
         if not match: return None
-        t, lo, q, hi, word, resp = match.groups()
+        lo, q, hi, word, resp = match.groups()
         canon_resp = resp
         for code in ('after', 'before', 'it'):
             if code.startswith(resp.lower()):
                 canon_resp = code
                 break
-        return cls(float(t), int(lo), int(q), int(hi), word, canon_resp)
+        return cls(t, int(lo), int(q), int(hi), word, canon_resp)
 
 @final
 @dataclass
@@ -61,9 +58,7 @@ class Prompt:
     resp: str
 
     pattern = re.compile(r'''(?x)
-        T (?P<time> \d+ (?: \. \d+ )? )
-
-        \s+
+        \s*
         >
         \s*
 
@@ -73,38 +68,55 @@ class Prompt:
     ''')
 
     @classmethod
-    def match(cls, line: str):
+    def match(cls, t: float, line: str):
         match = cls.pattern.match(line)
         if not match: return None
-        t, resp = match.groups()
-        return cls(float(t), resp)
+        (resp,) = match.groups()
+        return cls(t, resp)
 
-def analyze(lines: Iterable[str]):
+@final
+class SearchLog:
+    pattern = re.compile(r'''(?x)
+        T (?P<time> \d+ (?: \. \d+ )? )
+
+        \s*
+
+        (?P<line> .* )
+
+        $
+    ''')
+
     quest: list[Questioned] = []
     prompt: list[Prompt] = []
 
-    for line in lines:
-        line = line.rstrip('\r\n')
+    def __init__(self, lines: Iterable[str]):
+        for line in lines:
+            line = line.rstrip('\r\n')
 
-        q = Questioned.match(line)
-        if q is not None:
-            quest.append(q)
-            continue
+            match = self.pattern.match(line)
+            if not match: continue
+            time, line = match.groups()
+            t = float(time)
 
-        p = Prompt.match(line)
-        if p is not None:
-            prompt.append(p)
-            continue
+            q = Questioned.match(t, line)
+            if q is not None:
+                self.quest.append(q)
+                continue
 
-    quest = sorted(quest, key = lambda x: x.time)
-    prompt = sorted(prompt, key = lambda x: x.time)
+            p = Prompt.match(t, line)
+            if p is not None:
+                self.prompt.append(p)
+                continue
 
-    def merge():
+        self.quest = sorted(self.quest, key = lambda x: x.time)
+        self.prompt = sorted(self.prompt, key = lambda x: x.time)
+
+    def merge(self):
         qi = 0
         pi = 0
         while True:
-            qr = quest[qi] if qi < len(quest) else None
-            pr = prompt[pi] if pi < len(prompt) else None
+            qr = self.quest[qi] if qi < len(self.quest) else None
+            pr = self.prompt[pi] if pi < len(self.prompt) else None
 
             if qr is not None and pr is not None:
                 if qr.time < pr.time:
@@ -124,37 +136,43 @@ def analyze(lines: Iterable[str]):
 
             break
 
-    max_ix = max(max(r.lo, r.q, r.hi) for r in quest)
-    t_width = max(4, max(len(f'{r.time:.1f}') for r in quest))
-    ix_width = max(5, len(str(max_ix)))
-    word_width = max(6, max(len(r.word) for r in quest))
-    resp_width = max(8, max(len(r.resp) for r in quest))
+    def summary(self, legend: bool = True):
+        max_ix = max(max(r.lo, r.q, r.hi) for r in self.quest)
+        t_width = max(4, max(len(f'{r.time:.1f}') for r in self.quest))
+        ix_width = max(5, len(str(max_ix)))
+        word_width = max(6, max(len(r.word) for r in self.quest))
+        resp_width = max(8, max(len(r.resp) for r in self.quest))
 
-    prior_t = 0
+        prior_t = 0
 
-    yield f'T{"time":{t_width}} {"ΔT":>{t_width}} [ {"lo":{ix_width}} : {"query":{ix_width}} : {"hi":{ix_width}} ] {"<word>":{word_width}}? response ... analysis'
+        yield f'T{"time":{t_width}} {"ΔT":>{t_width}} [ {"lo":{ix_width}} : {"query":{ix_width}} : {"hi":{ix_width}} ] {"<word>":{word_width}}? response ... analysis'
 
-    for t, qn, pr in merge():
-        dt = t - prior_t
+        for t, qn, pr in self.merge():
+            dt = t - prior_t
 
-        if qn is not None:
-            w = qn.hi - qn.lo
-            m = math.floor(qn.hi/2+qn.lo/2)
-            b = qn.q - m
-            yield f'T{t:{t_width}.1f} {dt:{t_width}.1f} [ {qn.lo:{ix_width}} : {qn.q:{ix_width}} : {qn.hi:{ix_width}} ] {qn.word:{word_width}}? {qn.resp:{resp_width}} ... wid:{w:{ix_width}} mid:{m:{ix_width}} bias:{b}'
+            if qn is not None:
+                w = qn.hi - qn.lo
+                m = math.floor(qn.hi/2+qn.lo/2)
+                b = qn.q - m
+                yield f'T{t:{t_width}.1f} {dt:{t_width}.1f} [ {qn.lo:{ix_width}} : {qn.q:{ix_width}} : {qn.hi:{ix_width}} ] {qn.word:{word_width}}? {qn.resp:{resp_width}} ... wid:{w:{ix_width}} mid:{m:{ix_width}} bias:{b}'
 
-        elif pr is not None:
-            # TODO extract and report viewing window telemetry
-            yield f'T{t:{t_width}.1f} {dt:{t_width}.1f} > {pr.resp}'
+            elif pr is not None:
+                # TODO extract and report viewing window telemetry
+                yield f'T{t:{t_width}.1f} {dt:{t_width}.1f} > {pr.resp}'
 
-        prior_t = t
+            prior_t = t
 
-    yield ''
-    yield 'analysis legend:'
-    yield '* wid -- search window width, aka `hi-lo`'
-    yield '* mid -- classic binary search midpoint, aka `hi/2+lo/2`'
-    yield '* bias -- prefix seeking bias applied, aka `query-mid`'
+        if legend:
+            yield ''
+            yield 'analysis legend:'
+            yield '* wid -- search window width, aka `hi-lo`'
+            yield '* mid -- classic binary search midpoint, aka `hi/2+lo/2`'
+            yield '* bias -- prefix seeking bias applied, aka `query-mid`'
+
+def analyze(lines: Iterable[str]):
+    return SearchLog(lines).summary()
 
 if __name__ == '__main__':
-    for line in analyze(sys.stdin):
+    log = SearchLog(sys.stdin)
+    for line in log.summary():
         print(line)
