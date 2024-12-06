6f2e27b7ec81ae51c9e80770bcd0a758bde17856
Joshua T Corbin <jcorbin@wunjo.org>
2024-12-05 20:35:32 -0500

    semantic: add /http command

--- semantic.py
+++ semantic.py
@@ -9,7 +9,7 @@ import ollama
 import re
 import requests
 from collections import Counter
-from collections.abc import Generator, Iterable, Sequence
+from collections.abc import Generator, Iterable, MutableMapping, Sequence
 from dataclasses import dataclass
 from dateutil.tz import gettz
 from typing import assert_never, cast, final, overload, override, Callable, Literal
@@ -1252,6 +1252,53 @@ class Search(StoredLog):
             n = sum(1 for _ in words())
             yield f'{tier} {n:>{cw}}'
 
+    def do_http(self, ui: PromptUI):
+        title: str
+        coll: MutableMapping[str, str]|MutableMapping[str, str|bytes]
+
+        if ui.tokens.have(r'co(o(k(ie?)?)?)?$'):
+            title = 'cookie'
+            coll = self.http_client.cookies
+
+        elif ui.tokens.have(r'he(a(d(er?)?$)?)?'):
+            title = 'header'
+            coll = self.http_client.headers
+
+        else:
+            ui.print('Usage: /http {cookie|header}')
+            return
+
+        name = next(ui.tokens, None)
+        if name is None:
+            ui.print(f'http {title}s:')
+            for name, value in coll.items():
+                ui.print(f'- {name}: {json.dumps(value)}')
+            return
+
+        if ui.tokens.empty:
+            value = coll.get(name)
+            ui.print(f'http {title} {name}: {json.dumps(value)}')
+            return
+
+        valstr = next(ui.tokens)
+        if valstr == '_':
+            ui.log(f'http {title}: {name} _')
+            del coll[name]
+            ui.print(f'http {title} {name}: <deleted>')
+            return
+
+        try:
+            value = cast(object, json.loads(valstr))
+            assert isinstance(value, str)
+        except Exception as e:
+            ui.print(f'! bad {title} value: {e}')
+            return
+
+        vs = json.dumps(value)
+        ui.log(f'http {title}: {name} {vs}')
+        coll[name] = value
+        ui.print(f'http {title} {name}: <set> {vs}')
+
     @property
     def origin(self):
         origin = self.site
@@ -1373,6 +1420,8 @@ class Search(StoredLog):
         cmds: dict[str, PromptUI.State] = {
             # TODO proper help command ; reuse for '?' token
             # TODO add '/' prefix here ; generalize dispatch
+
+            'http': self.do_http,
 
             'prog': self.show_prog,
             'tiers': self.show_tiers,
