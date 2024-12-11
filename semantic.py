df8377f45f153e9cf3be82d888f2f0c8fd777a9e
Joshua T Corbin <jcorbin@wunjo.org>
2024-12-11 15:14:09 -0500

    TODO semantic such req error

--- semantic.py
+++ semantic.py
@@ -1610,6 +1610,10 @@ class Search(StoredLog):
             except requests.RequestException as e:
                 err = e
                 if verbose:
+                    strerror = getattr(err, 'strerror', None)
+                    errno = getattr(err, 'errno', None)
+                    ui.print(f'! errno:{errno!r}')
+                    ui.print(f'! strerror:{strerror!r}') # TODO collapse once we sort out how to hold it
                     ui.print(f'! {err}')
                 continue
 
