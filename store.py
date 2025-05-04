23ae63b364510681ba39bb095b8dc8dd5518ee94
Joshua T Corbin <jcorbin@wunjo.org>
2025-05-04 15:54:12 -0400

    store: add log_start

--- store.py
+++ store.py
@@ -82,6 +82,7 @@ class StoredLog:
         self.puzzle_id: str = ''
         self.sessions: list[LogSession] = []
         self.loaded: bool = False
+        self.log_start: datetime.datetime|None = None
 
     @property
     def expire(self) -> datetime.datetime|None:
@@ -303,6 +304,8 @@ class StoredLog:
                             assert prior_then_t is not None
                             dur = prior_t - prior_then_t
                         self.sessions.append(LogSession(prior_then, datetime.timedelta(seconds=dur)))
+                    if self.log_start is None:
+                        self.log_start = then
                     prior_then = then
                     prior_then_t = t
                     prior_t, cur_t = None, t
@@ -460,6 +463,7 @@ class StoredLog:
             with open(self.log_file, 'a') as f:
                 with ui.deps(log_file=f) as ui:
                     now = datetime.datetime.now(tzlocal())
+                    self.log_start = now
                     ui.log(f'now: {now:{self.dt_fmt}}')
                     yield self, ui
         finally:
