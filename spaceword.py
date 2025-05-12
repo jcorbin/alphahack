f4814b1b47fa80f34558c2c94323ef941ab95afe
Joshua T Corbin <jcorbin@wunjo.org>
2025-05-12 10:00:15 -0400

    TODO hist table

--- spaceword.py
+++ spaceword.py
@@ -2132,6 +2132,7 @@ class Search:
             elif not data:
                 ui.print('-- empty --')
             else:
+                # TODO maybe do a table
                 for n, h in enumerate(data, 1):
                     ui.print(f'{n}. {plain_dictify(h)!r}')
             return
