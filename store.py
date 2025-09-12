614456896429a97381fdcaa580edff996321152d
Joshua T Corbin <jcorbin@wunjo.org>
2025-09-12 23:34:06 +0000

    TODO store: cmd_site_link set

--- store.py
+++ store.py
@@ -326,6 +326,13 @@ class StoredLog:
         '''
         present puzzle site hyperlink... or "copy" to clipboard
         '''
+
+        # TODO if ui.tokens -> set and log?
+        #     site = next(tokens, None)
+        #     if site:
+        #         self.site = site
+        #         ui.log(f'site: {self.site}')
+
         label = self.site_name or self.site
         url = self.site
         if '://'not in url:
