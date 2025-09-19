2025f00dfed58162ce807af4cc43bd50dac0084c
Joshua T Corbin <jcorbin@wunjo.org>
2025-09-19 21:10:37 +0000

    sync: fetch then upadate-and-rebase

--- sync.sh
+++ sync.sh
@@ -2,8 +2,16 @@
 set -eux
 
 git checkout dev
-git fetch -f origin main:main
-git fetch -f origin rc:rc
-git fetch -f origin testing:testing
-git fetch -f origin daily:daily
-git pull origin dev
+git fetch -fp origin
+
+git update-ref -m sync --stdin <<EOF
+start
+update refs/heads/main origin/main
+update refs/heads/rc origin/rc
+update refs/heads/testing origin/testing
+update refs/heads/daily origin/daily
+prepare
+commit
+EOF
+
+git rebase origin/dev dev
