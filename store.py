afdd4688fa1e5598722383aa4f6e2835e138e235
Joshua T Corbin <jcorbin@wunjo.org>
2025-10-24 16:46:31 +0000

    TODO store: rebase StoredLog over Arguable

--- store.py
+++ store.py
@@ -262,6 +262,8 @@ def matcher(pat: str|re.Pattern[str]):
         return Matcher(pat, then)
     return inner
 
+# TODO rebase over PromptUI.Arguable
+
 OrderLogsBy = Literal['mtime','name']
 
 class StoredLog:
